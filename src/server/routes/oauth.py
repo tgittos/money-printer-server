55b120aa5e11fc42ad850397e762f05f39e08d72
Tim Gittos <tim@chromasplosion.com>
2021-08-19 09:03:36 -0500

    More refactoring, trying to get oauth for Plaid working

---
+++ src/server/routes/oauth.py
@@ -0,0 +1,56 @@
+@app.route('/v1/api/plaid/info', methods=['POST'])
+def info():
+    global access_token
+    global item_id
+    return jsonify({
+        'item_id': item_id,
+        'access_token': access_token,
+        'products': PLAID_PRODUCTS
+    })
+
+@app.route('/v1/api/plaid/create_link_token', methods=['POST'])
+def create_link_token():
+    try:
+        request = LinkTokenCreateRequest(
+            products=products,
+            client_name="Plaid Quickstart",
+            country_codes=list(map(lambda x: CountryCode(x), PLAID_COUNTRY_CODES)),
+            language='en',
+            user=LinkTokenCreateRequestUser(
+                client_user_id=str(time.time())
+            )
+        )
+
+        # create link token
+        response = client.link_token_create(request)
+        return jsonify(response.to_dict())
+    except plaid.ApiException as e:
+        return json.loads(e.body)
+
+@app.route('/v1/api/plaid/set_access_token', methods=['POST'])
+def get_access_token():
+    global access_token
+    global item_id
+    public_token = request.form['public_token']
+    try:
+        exchange_request = ItemPublicTokenExchangeRequest(
+            public_token=public_token)
+        exchange_response = client.item_public_token_exchange(exchange_request)
+        access_token = exchange_response['access_token']
+        item_id = exchange_response['item_id']
+        return jsonify(exchange_response.to_dict())
+    except plaid.ApiException as e:
+        return json.loads(e.body)
+
+@app.route('/v1/api/plaid/auth', methods=['GET'])
+def get_auth():
+    try:
+       request = AuthGetRequest(
+            access_token=access_token
+        )
+       response = client.auth_get(request)
+       pretty_print_response(response.to_dict())
+       return jsonify(response.to_dict())
+    except plaid.ApiException as e:
+        error_response = format_error(e)
+        return jsonify(error_response)
--- src/server/server.py
+++ src/server/server.py
@@ -4,18 +4,17 @@ sys.path.append('./../src')
 from flask import Flask, send_from_directory
 from flask_cors import CORS
 
-import scheduler
-
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.url_map.strict_slashes = False
 
 CORS(app)
 
+from config import DevelopmentConfig
 from routes import *
 
 if __name__ == '__main__':
-    # boot the scheduler
-    scheduler.start()
     # run Flask app
-    app.run()
+    print(" * running money-printer server with config: {0}".format(DevelopmentConfig))
+    app.config.from_object(DevelopmentConfig)
+    app.run(host=DevelopmentConfig.HOST, port=DevelopmentConfig.PORT)
