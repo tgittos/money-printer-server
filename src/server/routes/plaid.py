0057a4084e7edd784dacc690e447d922b28e5336
Tim Gittos <hi@timgittos.com>
2021-08-26 11:52:40 -0500

    Refactor the Plaid API a little

--- src/server/routes/plaid.py
+++ src/server/routes/plaid.py
@@ -1,41 +1,49 @@
 from flask import Blueprint
 from flask import request
 
-from core.apis.plaid import oauth as plaid_oauth
+from core.apis.plaid.common import PlaidApiConfig
+from core.apis.plaid.oauth import Oauth, OauthConfig
+from core.apis.plaid.auth import Auth, AuthConfig
+
 
 # load the env config
 from server import load_config
 server_config = load_config()
 
 # define a plaid oauth client config
-plaid_config = plaid_oauth.OauthConfig()
-plaid_config.product_name = "Money Printer"
+plaid_config = PlaidApiConfig()
 plaid_config.env = server_config['plaid']['env']
 plaid_config.client_id = server_config['plaid']['client_id']
 plaid_config.secret = server_config['plaid']['secret']
-plaid_config.mysql_config = server_config['db']
+
+oauth_config = OauthConfig()
+oauth_config.mysql_config = server_config['db']
+oauth_config.plaid_config = plaid_config
+
+auth_config = AuthConfig()
+auth_config.mysql_config = server_config['db']
+auth_config.plaid_config = plaid_config
+
 
 # define the blueprint for plaid oauth
 oauth_bp = Blueprint('plaid_oauth', __name__)
 
 @oauth_bp.route('/v1/api/plaid/info', methods=['POST'])
 def info():
-    client = plaid_oauth.Oauth(plaid_config)
+    client = Oauth(oauth_config)
     result_json = client.info()
     return result_json
 
 @oauth_bp.route('/v1/api/plaid/create_link_token', methods=['POST'])
 def create_link_token():
-    client = plaid_oauth.Oauth(plaid_config)
+    client = Oauth(oauth_config)
     result_json = client.create_link_token()
     return result_json
 
 @oauth_bp.route('/v1/api/plaid/set_access_token', methods=['POST'])
 def get_access_token():
-    global access_token
-    global item_id
     public_token = request.form['public_token']
-    client = plaid_oauth.Oauth(plaid_config)
+    client = Oauth(oauth_config)
     result_json = client.get_access_token(public_token)
     print("result_json: {0}".format(result_json))
     access_token = result_json['access_token']
@@ -44,6 +52,6 @@ def get_access_token():
 
 @oauth_bp.route('/v1/api/plaid/auth', methods=['GET'])
 def get_auth():
-    client = plaid_oauth.Oauth(plaid_config)
+    client = Auth(auth_config)
     result_json = client.get_auth(access_token)
     return result_json
