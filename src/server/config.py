a9abb933e64bf045742134c4906b2e05ff9496e6
Tim Gittos <tim@chromasplosion.com>
2021-09-25 09:45:33 -0500

    adding server config

---
+++ src/server/config.py
@@ -0,0 +1,41 @@
+import os
+from dynaconf import Dynaconf
+
+from core.apis.mailgun import MailGunConfig
+from core.stores.mysql import MySqlConfig
+
+env = os.environ['MP_ENVIRONMENT']
+
+config = Dynaconf(
+    envvar_prefix="MP",
+    core_loaders=['json'],
+    default_env="development",
+    environments=True,
+    env_switcher="MP_ENVIRONMENT",
+    settings_files=['config.json', '.secrets.json']
+)
+
+redis_config = config.redis
+
+mysql_config = MySqlConfig(
+    host=config.db.host,
+    port=config.db.port,
+    username=config.db.username,
+    password=config.db.password,
+    schema=config.db.schema
+)
+
+iex_config = {
+    'secret': config.iex.secret
+}
+
+# define a plaid oauth client config
+plaid_config = config.plaid
+
+mailgun_config = MailGunConfig(
+    domain=config.mailgun.domain,
+    api_key=config.mailgun.api_key
+)
+
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `settings_files` = Load these files in the order.
