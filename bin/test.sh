79c0a8df636c843b3cf5fa3c74968ab11bd6167f
Tim Gittos <hi@timgittos.com>
2021-12-18 11:37:14 -0600

    Adds coverage reporting to test runs

--- bin/test.sh
+++ bin/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec --tty $(docker-compose ps -q api) /bin/bash -c "MP_ENVIRONMENT=test PYTHONPATH=src pytest src/"
+docker exec --tty $(docker compose ps -q api) /bin/bash -c "MP_ENVIRONMENT=test PYTHONPATH=src pytest --cov-config=.coveragerc --cov=src --cov-report term:skip-covered src/"
