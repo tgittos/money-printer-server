c090d39c255e7c08fd4c7e6b28612ca2d10172ad
Tim Gittos <hi@timgittos.com>
2021-12-19 12:03:14 -0600

    Filling in tests, hitting serialization bs

--- src/api/routes/scheduler_test.py
+++ src/api/routes/scheduler_test.py
@@ -1,3 +1,5 @@
+from rq import job
+from werkzeug.datastructures import Authorization
 import pytest
 import json
 
@@ -5,6 +7,7 @@ from tests.helpers import client, db
 from tests.factories import create_scheduled_job, create_user_profile
 
 from core.models.scheduler.scheduled_job import ScheduledJob
+from core.lib.actions.scheduled_job.requests import CreateScheduledJobRequest, CreateInstantJobRequest
 from core.lib.jwt import encode_jwt
 from api.routes.scheduler import ScheduledJobRepository
 
@@ -43,22 +46,67 @@ def user_token(db):
     session.close()
     return token
 
+@pytest.fixture()
+def valid_scheduled_job_request():
+    return CreateScheduledJobRequest(
+        job_name="My New Job",
+        cron="0 * * * *",
+        args={}
+    )
+
+@pytest.fixture()
+def invalid_scheduled_job_request():
+    return CreateScheduledJobRequest(
+        job_name="",
+        cron=None,
+        args=""
+    )
+
+@pytest.fixture()
+def valid_instant_job_request():
+    return CreateInstantJobRequest(
+        job_name= "Once off job",
+        args={}
+    )
+
+@pytest.fixture()
+def invalid_instant_job_request():
+    return CreateInstantJobRequest(
+        job_name="",
+        args=None
+    )
+
 
 # /v1/api/admin/schedules
-def test_create_schedule_accepts_valid_input(client):
-    assert False
+def test_create_schedule_accepts_valid_input_for_admin(client, admin_token, valid_scheduled_job_request):
+    response = client.post('/v1/api/admin/schedules',
+                           headers={'Authorization': f"Bearer {admin_token}"},
+                           json=json.dumps(valid_scheduled_job_request))
+    assert response.status_code == 201
+    response_json = response.get_json()
+    assert len(response_json['data'] == 1)
+    assert response_json[0]['id'] is not None
+    assert response_json[0]['job_name'] == valid_scheduled_job_request.job_name
 
 
 # /v1/api/admin/schedules
-def test_create_schedule_rejects_invalid_input(client):
-    assert False
+def test_create_schedule_rejects_invalid_input(client, admin_token, invalid_scheduled_job_request):
+    response = client.post('/v1/api/admin/schedules',
+                           headers={'Authorization': f"Bearer {admin_token}"},
+                           json=json.dumps(invalid_scheduled_job_request))
+    assert response.status_code == 400
 
+def test_create_schedule_rejects_valid_input_for_non_admin(client, user_token, valid_scheduled_job_request):
+    response = client.post('/v1/api/admin/schedules',
+                           headers={'Authorization': f"Bearer {user_token}"},
+                           json=json.dumps(valid_scheduled_job_request))
+    assert response.status_code == 401
 
 # /v1/api/admin/schedules/1
 def test_list_schedules_returns_all_schedules_for_admin(db, client, admin_token):
-    session = db.get_session()
-    job_1 = create_scheduled_job(session)
-    job_2 = create_scheduled_job(session)
+    with db.get_session() as session:
+        job_1 = create_scheduled_job(session)
+        job_2 = create_scheduled_job(session)
     response = client.get('/v1/api/admin/schedules',
                           headers={'Authorization': f"Bearer {admin_token}"},
                           follow_redirects=True)
@@ -68,39 +116,36 @@ def test_list_schedules_returns_all_schedules_for_admin(db, client, admin_token)
     for json in response_json['data']:
         assert json['id'] in [job_1.id, job_2.id]
         assert json['job_name'] in [job_1.job_name, job_2.job_name]
-    session.close()
 
 
 # /v1/api/admin/schedules/1
 def test_list_schedules_returns_401_for_non_admin(db, client, user_token):
-    session = db.get_session()
-    job_1 = create_scheduled_job(session)
-    job_2 = create_scheduled_job(session)
+    with db.get_session() as session:
+        job_1 = create_scheduled_job(session)
+        job_2 = create_scheduled_job(session)
     response = client.get('/v1/api/admin/schedules',
                           headers={'Authorization': f"Bearer {user_token}"},
                           follow_redirects=True)
     assert response.status_code == 401
-    session.close()
 
 
 # /v1/api/admin/schedules/1
 def test_update_schedule_accepts_valid_input_for_admin_token(db, client, admin_token):
-    session = db.get_session()
-    job = create_scheduled_job(session)
-    session.close()
+    with db.get_session() as session:
+        job = create_scheduled_job(session)
+
     assert job.job_name != 'Updated Job Name'
     response = client.put('/v1/api/admin/schedules/' + str(job.id),
-                          headers={'Authorization': f"Bearer {admin_token}"},
-                          data=json.dumps({
-                              'job_name':'Updated Job Name'
-                          }),
-                          content_type='application/json',
-                          follow_redirects=True)
+                        headers={'Authorization': f"Bearer {admin_token}"},
+                        data=json.dumps({
+                            'job_name':'Updated Job Name'
+                        }),
+                        content_type='application/json',
+                        follow_redirects=True)
     assert response.status_code == 200
-    session = db.get_session()
-    updated_job = session.query(ScheduledJob).where(ScheduledJob.id == job.id).first()
-    assert updated_job.job_name == 'Updated Job Name'
-    session.close()
+    with db.get_session() as session:
+        updated_job = session.query(ScheduledJob).where(ScheduledJob.id == job.id).first()
+        assert updated_job.job_name == 'Updated Job Name'
 
 
 def updating_a_schedule_reschedules_job():
@@ -109,8 +154,8 @@ def updating_a_schedule_reschedules_job():
 
 # /v1/api/admin/schedules/1
 def test_update_schedule_returns_401_for_user_token(db, client, user_token):
-    session = db.get_session()
-    job = create_scheduled_job(session)
+    with db.get_session() as session:
+        job = create_scheduled_job(session)
     assert job.job_name != 'Updated Job Name'
     response = client.put('/v1/api/admin/schedules/' + str(job.id),
                           headers={'Authorization': f"Bearer {user_token}"},
@@ -120,9 +165,9 @@ def test_update_schedule_returns_401_for_user_token(db, client, user_token):
                           content_type='application/json',
                           follow_redirects=True)
     assert response.status_code == 401
-    updated_job = session.query(ScheduledJob).where(ScheduledJob.id == job.id).first()
+    with db.get_session() as session:
+        updated_job = session.query(ScheduledJob).where(ScheduledJob.id == job.id).first()
     assert updated_job.job_name == job.job_name
-    session.close()
 
 # /v1/api/admin/schedules/1
 def test_update_schedule_rejects_invalid_input(client):
