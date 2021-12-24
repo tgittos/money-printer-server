16216bf87d137b75f20158f24a18bdc5a03aefaa
Tim Gittos <tim@chromasplosion.com>
2021-12-24 16:01:56 -0600

    Integrated Matomo and Prometheus into the docker stack

--- bin/dev/init-db.sh
+++ bin/dev/init-db.sh
@@ -5,9 +5,13 @@ source .env.dev
 echo "Creating user $MP_DB__USERNAME in db mp_dev"
 docker compose run --rm db mysql --user=root --password=$MYSQL_ROOT_PASSWORD --execute="CREATE USER '$MP_DB__USERNAME' IDENTIFIED BY '$MP_DB__PASSWORD'"
 
-echo "Creating db mp_dev and granting access to $MP_DB__USERNAME"
+echo "Creating db mp_dev"
 docker compose run --rm db mysql --user=root --password=$MYSQL_ROOT_PASSWORD --execute="CREATE SCHEMA mp_dev;"
+echo "Creating db mp_test"
 docker compose run --rm db mysql --user=root --password=$MYSQL_ROOT_PASSWORD --execute="CREATE SCHEMA mp_test;"
+echo "Creating db mp_matomo"
+docker compose run --rm db mysql --user=root --password=$MYSQL_ROOT_PASSWORD --execute="CREATE SCHEMA mp_matomo;"
+echo "Granting access to dbs to $MP_DB__USERNAME"
 docker compose run --rm db mysql --user=root --password=$MYSQL_ROOT_PASSWORD --database mp_dev --execute="GRANT ALL PRIVILEGES ON *.* TO '$MP_DB__USERNAME';"
 
 echo "Migrating db"
