0381b62376b26439495011c130118b6cb00073d7
Tim Gittos <hi@timgittos.com>
2021-12-26 15:27:25 -0600

    Tests run in parallel, a bunch fail now

--- src/tests/fixtures/core.py
+++ src/tests/fixtures/core.py
@@ -1,6 +1,7 @@
 import pytest
 import os
-from sqlalchemy_utils import database_exists
+from sqlalchemy import inspect
+from sqlalchemy.sql import text
 
 from config import mysql_config
 from core.stores.mysql import MySql
@@ -14,13 +15,11 @@ os.environ["MP_ENVIRONMENT"] = "test"
 @pytest.fixture(scope='session')
 def db(autouse=True):
     db = MySql(mysql_config)
-    # if not database_exists(db.engine.url):
-    #    db.create_all()
-    db.create_all()
+    inspector = inspect(db.engine)
+    if not inspector.has_table('alembic_version'):
+        db.create_all()
     yield db
-    # if database_exists(db.engine.url):
-    #    db.drop_all()
-    db.drop_all()
+    truncate_db(db)
 
 
 # one API for the the whole test session so that we can parallelize it
@@ -31,3 +30,24 @@ def client(db):
     with app.flask_app.test_client() as client:
         with app.flask_app.app_context():
             yield client
+
+def truncate_db(db):
+    script = """EOF
+        SET FOREIGN_KEY_CHECKS = 0;
+
+        SELECT @str := CONCAT('TRUNCATE TABLE ', table_schema, '.', table_name, ';')
+        FROM   information_schema.tables
+        WHERE  table_type   = 'BASE TABLE'
+        AND  table_schema IN ('db1_name','db2_name');
+
+        PREPARE stmt FROM @str;
+
+        EXECUTE stmt;
+
+        DEALLOCATE PREPARE stmt;
+
+        SET FOREIGN_KEY_CHECKS = 1;
+    """
+    statement = text(script)
+    with db.engine.connect() as connection:
+        connection.execute(statement)
