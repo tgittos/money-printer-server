543d8d17f86ba704014ce1d8ca90b8a4ac1d0a61
Tim Gittos <hi@timgittos.com>
2021-12-26 17:53:57 -0600

    Switching computers

--- src/tests/fixtures/core.py
+++ src/tests/fixtures/core.py
@@ -1,7 +1,8 @@
 import pytest
 import os
-from sqlalchemy import inspect
+from sqlalchemy import inspect, MetaData
 from sqlalchemy.sql import text
+import contextlib
 
 from config import mysql_config
 from core.stores.mysql import MySql
@@ -15,11 +16,7 @@ os.environ["MP_ENVIRONMENT"] = "test"
 @pytest.fixture(scope='session')
 def db(autouse=True):
     db = MySql(mysql_config)
-    inspector = inspect(db.engine)
-    if not inspector.has_table('alembic_version'):
-        db.create_all()
     yield db
-    truncate_db(db)
 
 
 # one API for the the whole test session so that we can parallelize it
@@ -30,24 +27,3 @@ def client(db):
     with app.flask_app.test_client() as client:
         with app.flask_app.app_context():
             yield client
-
-def truncate_db(db):
-    script = """EOF
-        SET FOREIGN_KEY_CHECKS = 0;
-
-        SELECT @str := CONCAT('TRUNCATE TABLE ', table_schema, '.', table_name, ';')
-        FROM   information_schema.tables
-        WHERE  table_type   = 'BASE TABLE'
-        AND  table_schema IN ('db1_name','db2_name');
-
-        PREPARE stmt FROM @str;
-
-        EXECUTE stmt;
-
-        DEALLOCATE PREPARE stmt;
-
-        SET FOREIGN_KEY_CHECKS = 1;
-    """
-    statement = text(script)
-    with db.engine.connect() as connection:
-        connection.execute(statement)
