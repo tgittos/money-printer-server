e6314ca94f8b493bdfe0294c9c9c9f7f3bf8f31a
Tim Gittos <hi@timgittos.com>
2021-12-28 14:53:15 -0600

    switching computers

--- src/core/actions/account/test_account_crud.py
+++ src/core/actions/account/test_account_crud.py
@@ -1,81 +1,51 @@
 import pytest
 
 
-@pytest.mark.skip(reason="need to go back and implement this")
 def test_create_account_accepts_valid_input():
     assert False
 
 
-@pytest.mark.skip(reason="need to go back and implement this")
 def test_update_account_accepts_valid_input():
     assert False
 
 
-@pytest.mark.skip(reason="need to go back and implement this")
 def test_create_or_update_account_creates_account_if_missing():
     assert False
 
 
-@pytest.mark.skip(reason="need to go back and implement this")
 def test_create_or_update_account_updates_account_if_present():
     assert False
 
 
-@pytest.mark.skip(reason="need to go back and implement this")
 def test_get_account_by_id_returns_account():
     assert False
 
 
-@pytest.mark.skip(reason="need to go back and implement this")
 def test_get_account_id_fails_for_missing_account():
     assert False
 
 
-@pytest.mark.skip(reason="need to go back and implement this")
-def test_get_account_id_fails_for_account_in_another_profile():
-    assert False
 
-
-@pytest.mark.skip(reason="need to go back and implement this")
 def test_get_account_by_account_id_returns_account():
     assert False
 
 
-@pytest.mark.skip(reason="need to go back and implement this")
 def test_get_account_account_id_fails_for_missing_account():
     assert False
 
 
-@pytest.mark.skip(reason="need to go back and implement this")
-def test_get_account_account_id_fails_for_account_in_another_profile():
-    assert False
-
-
-@pytest.mark.skip(reason="need to go back and implement this")
 def test_get_account_by_profile_returns_account():
     assert False
 
 
-@pytest.mark.skip(reason="need to go back and implement this")
 def test_get_account_profile_fails_for_missing_account():
     assert False
 
 
-@pytest.mark.skip(reason="need to go back and implement this")
-def test_get_account_profile_fails_for_account_in_another_profile():
-    assert False
-
-
-@pytest.mark.skip(reason="need to go back and implement this")
 def test_get_account_by_plaid_item_returns_account():
     assert False
 
 
-@pytest.mark.skip(reason="need to go back and implement this")
 def test_get_account_plaid_item_fails_for_missing_account():
     assert False
 
-
-@pytest.mark.skip(reason="need to go back and implement this")
-def test_get_account_plaid_item_fails_for_account_in_another_profile():
-    assert False
