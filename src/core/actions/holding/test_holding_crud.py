aac04c7cb28f8a9707c7a9d1a8e34661c4b7690e
Tim Gittos <hi@timgittos.com>
2022-01-02 13:42:00 -0600

    Progress

--- src/core/actions/holding/test_holding_crud.py
+++ src/core/actions/holding/test_holding_crud.py
@@ -43,24 +43,52 @@ def test_get_holding_by_id_returns_holding(db, account_factory, valid_create_hol
     assert result.data.id is not None
 
 
-def test_get_holding_by_id_fails_for_missing_holding():
-    assert False
+def test_get_holding_by_id_fails_for_missing_holding(db):
+    result = get_holding_by_id(db, 24234)
+    assert not result.success
+    assert result.data is None
 
 
-def test_get_holdings_by_account_id_returns_holdings_for_account():
-    assert False
+def test_get_holdings_by_account_id_returns_holdings_for_account(db, account_factory, holding_factory):
+    account = account_factory()
+    holding_1 = holding_factory(account_id=account.id)
+    holding_2 = holding_factory(account_id=account.id)
+    result = get_holdings_by_account_id(db, account_id=account.id)
+    assert result.success
+    assert result.data is not None
+    assert len(result.data) == 2
+    ids = [d.id for d in result.data]
+    assert holding_1.id in ids
+    assert holding_2.id in ids
 
 
-def test_get_holdings_by_account_id_returns_empty_array_with_no_holdings():
-    assert False
+def test_get_holdings_by_account_id_returns_empty_array_with_no_holdings(db, account_factory):
+    account = account_factory()
+    result = get_holdings_by_account_id(db, account_id=account.id)
+    assert result.success
+    assert result.data is not None
+    assert len(result.data) == 0
 
 
-def test_get_holding_balances_by_holding_id_returns_balances():
-    assert False
+def test_get_holding_balances_by_holding_id_returns_balances(db, holding_factory, holding_balance_factory):
+    holding = holding_factory()
+    balance_1 = holding_balance_factory(holding_id=holding.id)
+    balance_2 = holding_balance_factory(holding_id=holding.id)
+    result = get_holding_balances_by_holding_id(db, holding_id=holding.id)
+    assert result.success
+    assert result.data is not None
+    assert len(result.data) == 2
+    ids = [d.id for d in result.data]
+    assert balance_1.id in ids
+    assert balance_2.id in ids
 
 
-def test_get_holding_balances_by_holding_id_returns_empty_with_no_balances():
-    assert False
+def test_get_holding_balances_by_holding_id_returns_empty_with_no_balances(db, holding_factory):
+    holding = holding_factory()
+    result = get_holding_balances_by_holding_id(db, holding_id=holding.id)
+    assert result.success
+    assert result.data is not None
+    assert len(result.data) == 0
 
 
 def test_create_holding_accepts_valid_input():
--- src/tests/fixtures/holding_fixtures.py
+++ src/tests/fixtures/holding_fixtures.py
@@ -1,6 +1,6 @@
 import pytest
 import random
-import random
+import string
 from datetime import datetime, timezone
 
 from core.models import Holding, HoldingBalance
@@ -41,16 +41,43 @@ def holding_factory(db, faker, account_factory, security_factory):
 
 
 @pytest.fixture
-def valid_create_holding_request_factory(account_factory):
+def holding_balance_factory(db, faker, holding_factory):
+    def __holding_balance_factory(
+        holding_id=None,
+        cost_basis=random.random() * 5,
+        quantity=random.randint(1, 500),
+    ):
+        if holding_id is None:
+            holding_id = holding_factory().id
+
+        with db.get_session() as session:
+            balance = HoldingBalance()
+
+            balance.holding_id = holding_id
+            balance.cost_basis = cost_basis
+            balance.quantity = quantity
+            balance.timestamp = datetime.now(tz=timezone.utc)
+
+            session.add(balance)
+            session.commit()
+
+            return balance
+    return __holding_balance_factory
+
+
+@pytest.fixture
+def valid_create_holding_request_factory(account_factory, security_factory):
     def __valid_create_holding_request_factory(
         account_id=None,
-        security_symbol=id_generator(3, chars=string.ascii_uppercase),
+        security_symbol=None,
         cost_basis=random.random() * 5,
         quantity=random.randint(1, 500),
         iso_currency_code='USD'
     ):
         if account_id is None:
             account_id = account_factory().id
+        if security_symbol is None:
+            security_symbol = security_factory().symbol
 
         return CreateHoldingSchema().load({
             'account_id': account_id,
@@ -58,9 +85,26 @@ def valid_create_holding_request_factory(account_factory):
             'cost_basis': cost_basis,
             'quantity': quantity,
             'iso_currency_code': iso_currency_code,
-            'timestamp': datetime.now(tz=timezone.utc)
         })
     return __valid_create_holding_request_factory
+
+
+@pytest.fixture
+def valid_create_holding_balance_request_factory(holding_factory):
+    def __valid_create_holding_balance_request_factory(
+        holding_id=None,
+        cost_basis=random.random() * 5,
+        quantity=random.randint(1, 500),
+    ):
+        if holding_id is None:
+            holding_id = holding_factory().id
+        
+        return CreateHoldingBalanceSchema().load({
+            'holding_id': holding_id,
+            'cost_basis': cost_basis,
+            'quantity': quantity
+        })
+    return __valid_create_holding_balance_request_factory
 
 
 @pytest.fixture
