8c0fab781219e95d4457bb7de93c9a99b39a65cd
Tim Gittos <tim@chromasplosion.com>
2022-01-02 16:12:15 -0600

    Experiment

--- bin/dev/test.py
+++ bin/dev/test.py
@@ -14,12 +14,15 @@ if __name__ == '__main__':
     Base.metadata.drop_all(bind=db.engine)
     Base.metadata.create_all(bind=db.engine)
     print("Running tests...")
-    test_cmd = ["pytest", "-n", "auto", "--cov-config=.coveragerc", "--cov=src", "--cov-report", "term:skip-covered", "src/"]
+    test_cmd = ["pytest", "-n", "auto", "--cov-config=.coveragerc",
+                "--cov=src", "--cov-report", "term:skip-covered", "src/"]
     completed = subprocess.run(test_cmd)
     print("Dropping db table set...")
     Base.metadata.drop_all(bind=db.engine)
     print("Done")
     if 'MP_CI' in os.environ:
+        print(completed.stdout)
+        print(completed.stderr)
         completed.check_returncode()
     else:
         sys.exit(completed.returncode)
